87ccbe20600d846e942d4cd8f012443f09bd1f1b
JoelML-cyber <medinalizanaj@gmail.com>
2025-07-18 23:38:51 -0500

    Agrego procedure de login del empleado

--- SQL/procedures.sql
+++ SQL/procedures.sql
@@ -1,18 +1,30 @@
 
 -- Creacion de los procedures, aqui empiezan
 
--- PROCEDURE PARA EL LOGIN DEL EMPLEADO
-DELIMITER //
+-- Procedure para registrar intento fallido y bloquear si hay 3
 
-CREATE PROCEDURE ValidarEmpleadoLogin (
-    IN nombreCompleto VARCHAR(100),
-    IN claveAcceso VARCHAR(10)
-)
+DELIMITER $$
+
+CREATE PROCEDURE RegistrarIntentoFallido(IN p_tarjeta_id INT)
 BEGIN
-    SELECT * FROM EMPLEADOS
-    WHERE 
-        CONCAT(E_NOMBRE, ' ', E_APELLIDO) = nombreCompleto
-        AND E_CLAVE = claveAcceso;
-END //
+    DECLARE intentos INT;
+
+    -- Insertar intento fallido con la fecha de hoy
+    INSERT INTO INTENTOSFALLIDOS (TARJETA_ID, FECHAINTENTO)
+    VALUES (p_tarjeta_id, CURDATE());
+
+    -- Contar cuántos intentos tiene esa tarjeta hoy
+    SELECT COUNT(*) INTO intentos
+    FROM INTENTOSFALLIDOS
+    WHERE TARJETA_ID = p_tarjeta_id AND FECHAINTENTO = CURDATE();
+
+    -- Si son 3 o más, bloquear tarjeta
+    IF intentos >= 3 THEN
+        UPDATE TARJETA
+        SET TARJETA_ESTADO = 'bloqueada'
+        WHERE TARJETA_ID = p_tarjeta_id;
+    END IF;
+END $$
 
 DELIMITER ;
+
